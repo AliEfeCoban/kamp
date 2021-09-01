53d85a2fb9dbe2c1adc083b3f9db6e26197a1027
Ali Efe Çoban <Ali Efe oban@DESKTOP-TCHU6EJ>
2021-09-01 21:45:40 +0300

    Add project files.

---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming covertion
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Tebrikler Sepete Ekledi : " + urun.Adi );
+        }
+        public void Ekle2(string urunAdi, string acıklama, double fiyat)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi :"+ urunAdi);
+        }
+
+        }
+    }
+}
---
+++ kamp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace kamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //do not repeat yourself - Kendini tekrarlama
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 32000;
+            double faizOranı = 1.45 ;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu")
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("artş butonunu göster")
+            }
+            else
+            { Console.WriteLine("değişmedi butonu)}
+            if (sistemeGirisYapmisMi == true) ;
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+
+            else
+            {
+                Console.WriteLine("giriş yap butonu");
+
+            }
+            Console.WriteLine(kategoriEtiketi);
+            {
+
+            }
+
+            
+}
